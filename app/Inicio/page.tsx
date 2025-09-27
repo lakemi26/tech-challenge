eb2fae0a2d54e9ef8d1ddbced1087b9ef1c9a4ff
lakemi26 <lakemi@outlook.com.br>
2025-09-27 15:25:21 -0300

    feat: aplica títulos

--- app/Inicio/page.tsx
+++ app/Inicio/page.tsx
@@ -1,8 +1,13 @@
 import NewTransactionSection from "@/components/NewTransactionSection";
+import PageHeading from "@/components/PageHeading";
 
 export default function Inicio() {
   return (
     <div>
+      <PageHeading
+        title="Olá, {name}!"
+        subtitle="Gerencie suas finanças de forma simples e eficiente"
+      />
       <NewTransactionSection />
     </div>
   );
---
+++ app/Transacoes/page.tsx
@@ -0,0 +1,14 @@
+import PageHeading from "@/components/PageHeading";
+
+export default function Transacoes() {
+  return (
+    <div>
+      <div>
+        <PageHeading
+          title="Todas as transações"
+          subtitle="Gerencie e visualize todas as suas transações"
+        />
+      </div>
+    </div>
+  );
+}
